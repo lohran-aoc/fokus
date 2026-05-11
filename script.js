9f104ddc871a0964d544e8624d5c8dde44818354
lohran-aoc <lohran.aoc92@gmail.com>
2026-05-11 13:39:54 -0300

    Lista de exercícios

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const html = document.querySelector('html');
+const displayTempo = document.querySelector('#timer');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
+const botaoIniciar = document.querySelector('.app__card-primary-button');
+
+const duracaoFoco = 1500;
+const duracaoDescansoCurto = 300;
+const duracaoDescansoLongo = 900;
+
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
@@ -14,3 +23,4 @@ curtoBt.addEventListener('click', () => {
 longoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo')
 });
+
